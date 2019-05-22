02d7e574ad39202ad896376591459c20f51c6de9
Eduarda Ferreira <48859707+dudacf26@users.noreply.github.com>
2019-05-21 21:48:06 -0300

    Update ReceitaPudim.java

--- Receita/src/ReceitaPudim.java
+++ Receita/src/ReceitaPudim.java
@@ -4,7 +4,7 @@ public class ReceitaPudim implements IReceita {
     @Override
     public String receita() {
         // TODO Auto-generated method stub
-        return "Misturar os ingredientes; bater no liquidificador; colocar no forno por 1h";
+        return "Receita de Pudim";
     }
 
 }
