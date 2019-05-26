92ecf013d89898dd79d2173c52d967116e0f9593
Junit <ronaldo.valenci@gmail.com>
2019-05-26 02:35:54 -0300

    Update TesteReceita.java

--- Receita/src/TesteReceita.java
+++ Receita/src/TesteReceita.java
@@ -2,8 +2,11 @@
 public class TesteReceita {
 
 	public static void main(String[] args) {
-		IReceita bolo = new ReceitaBolo();
-		System.out.println(bolo.receita());
+		/*IReceita bolo = new ReceitaBolo();
+		System.out.println(bolo.receita());*/
+
+		IReceita veiculo = new Veiculo();
+		System.out.println(veiculo.receita());
 		
 		
 	}
