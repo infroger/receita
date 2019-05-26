2c2f0d424e910914ac6af0ff637e3fc16f353caa
Junit <ronaldo.valenci@gmail.com>
2019-05-26 02:50:28 -0300

    Add class and object.
    Add!

--- Receita/src/Veiculo.java
+++ Receita/src/Veiculo.java
@@ -2,6 +2,6 @@ public class Veiculo implements IReceita{
     @Override
     public String receita() {
 
-        return "Escolher o numero de rodas, numero de passageiros, tipo de tração e combustivel.";
+        return "Escolher o numero de rodas, numero de passageiros, tipo de tração e combustivel!";
     }
 }
