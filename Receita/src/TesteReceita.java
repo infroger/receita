df44109870f14beef749c6469a55d99aff23658a
THAMIRES ALVES TISSOT <ALU201717704@uniritter.local>
2019-05-28 20:10:23 -0300

    trabalho

--- Receita/src/TesteReceita.java
+++ Receita/src/TesteReceita.java
@@ -3,6 +3,7 @@ public class TesteReceita {
 
 	public static void main(String[] args) {
 		IReceita bolo = new ReceitaBolo();
+		IReceita bolo = new ReceitaPao();
 		System.out.println(bolo.receita());
 		
 		
