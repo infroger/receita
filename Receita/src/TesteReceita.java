9d4e6009fd2e40cf4daead22378f3d5425b2f479
THAMIRES ALVES TISSOT <ALU201717704@uniritter.local>
2019-05-28 20:18:39 -0300

    trab cuca

--- Receita/src/TesteReceita.java
+++ Receita/src/TesteReceita.java
@@ -4,8 +4,9 @@ public class TesteReceita {
 	public static void main(String[] args) {
 		IReceita bolo = new ReceitaBolo();
 		IReceita bolo = new ReceitaPao();
-		System.out.println(bolo.receita());
-		
+ IReceita cuca = new ReceitaCuca();		System.out.println(bolo.receita());
+		System.out.println(cuca.receita());
+
 		
 	}
 	
