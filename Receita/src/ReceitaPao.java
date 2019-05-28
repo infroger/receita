7a8656bece7021dbecbfe4b7a66a7a00b7d17a28
THAMIRES ALVES TISSOT <ALU201717704@uniritter.local>
2019-05-28 20:13:41 -0300

    trab

--- Receita/src/ReceitaPao.java
+++ Receita/src/ReceitaPao.java
@@ -4,7 +4,7 @@ public class ReceitaPao implements IReceita {
 	@Override
 	public String receita() {
 
-		return "Misturar os ingredientes; Adicionar fermento; Sovar; Ir ao forno por 40 min";
+		return "Misturar os ingredientes; Adicionar fermento; Sovar bem; Ir ao forno por 45 min";
 	}
 
 }
