d6a3f44ffeda0131e462c0b3d7c8f0419e50d816
EDUARDO PEREIRA BUJAK <ALU201616989@uniritter.local>
2019-05-28 19:18:06 -0300

    exercicio receita

---
+++ Receita/src/ReceitaMassaCarbonara.java
@@ -0,0 +1,16 @@
+
+public class ReceitaMassaCarbonara implements IReceita{
+
+	@Override
+	public String receita() {
+		// TODO Auto-generated method stub
+		return("Frite bem o bacon, até ficar crocante (pode-se adicionar salame picado).\r\n" + 
+				"Coloque o macarrão para cozinhar em água e sal.\r\n" + 
+				"No refratário onde será servido o macarrão, bata bem os ovos com um garfo.\r\n" + 
+				"Tempere com sal e pimenta a gosto, e junte o queijo ralado, também a gosto.\r\n" + 
+				"Quando o macarrão estiver pronto, escorra e coloque (bem quente) sobre a mistura de ovos, misture bem.\r\n" + 
+				"O calor da massa cozinha os ovos.\r\n" + 
+				"Coloque o bacon, ainda quente, sobre o macarrão e sirva.");
+	}
+
+}
