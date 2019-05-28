a8ebf6d9ea979e77ea54df19e29fb98cb5fc50b6
ALU201719628 <ALU201719628@F7-LAB201-1381.uniritter.local>
2019-05-28 19:59:40 -0300

    123

---
+++ Receita/src/ReceitaMonstro.java
@@ -0,0 +1,10 @@
+
+public class ReceitaBolo implements IReceita {
+	
+	@Override
+	public String receita() {
+		// TODO Auto-generated method stub
+		return "Frango e batata doce; 9 da matina; pronto pra ficar monstrão";
+	}
+
+}
